10b3506f18e70d3abd69acb2468a9de0c08a01c5
1mlvn <oskar.mlvn@gmail.com>
2022-01-31 16:29:34 +0100

    Inicializacja Grida

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import cloneDeep from "lodash.clonedeep";
+import { useEvent, getColors } from "./util";
 
 function App() {
-  return (
-    <div className="App">
+  const UP_ARROW = 38;
+  const DOWN_ARROW = 40;
+  const LEFT_ARROW = 37;
+  const RIGHT_ARROW = 39;
 
-    </div>
-  );
+  const [data, setData] = useState([
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ]);
+
+  const [gameOver, setGameOver] = useState(false);
+
+ const initialize = () => {
+
+    let newGrid = cloneDeep(data);
+    console.log(newGrid);
+
+    addNumber(newGrid);
+    console.table(newGrid);
+    addNumber(newGrid);
+    console.table(newGrid);
+    setData(newGrid);
+  };
+
+ 
+  const addNumber = (newGrid) => {
+    let added = false;
+    let gridFull = false;
+    let attempts = 0;
+    while (!added) {
+      if (gridFull) {
+        break;
+      }
+
+      let rand1 = Math.floor(Math.random() * 4);
+      let rand2 = Math.floor(Math.random() * 4);
+      attempts++;
+      if (newGrid[rand1][rand2] === 0) {
+        newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
+        added = true;
+      }
+      if (attempts > 50) {
+        gridFull = true;
+        let gameOverr = checkIfGameOver();
+        if (gameOverr) {
+          alert("game over");
+          
+        }
+       
+      }
+    }
+  };
 }
 
 export default App;
